feabc9c3cd294b15d0ac8e124a1485848988ae21
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-24 13:26:27 +0200

    add trains to fix

---
+++ laravel-migration-seeder/resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Train</title>
+</head>
+
+<body>
+    @forelse ($trains as $train)
+        <div class="singolo-treno">
+            <h2>
+                Treno: {{ $train->train_code }}
+            </h2>
+            <p>
+                partenza: {{ $train->departure_station }}
+            </p>
+            <p>
+                arrivo: {{ $train->arrival_station }}
+            </p>
+
+        </div>
+    @empty
+        Non ci sono Treni disponibili.
+    @endforelse
+</body>
+
+</html>
